fe2939e99d548cf8e374fc8b8e82660c57fe8990
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-09-04 13:46:15 +0200

    refactor(cli): use clap::Parser

--- src/main.rs
+++ src/main.rs
@@ -25,77 +25,45 @@
 
 use std::process;
 
-use clap::{App, Arg, ArgGroup};
+use clap::Parser;
 
 use diceware::{Config, EmbeddedList, Error};
 
+/// A Diceware passphrase generator.
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+struct Cli {
+    /// The number of words to generate.
+    words: usize,
+    /// Use a diceware word file.
+    #[clap(long = "file", short = 'f', group = "word_list")]
+    word_file: Option<String>,
+    /// Use the English embedded word list.
+    #[clap(long = "en", group = "word_list")]
+    english: bool,
+    /// Use the French embedded word list.
+    #[clap(long = "fr", group = "word_list")]
+    french: bool,
+    /// Add a special character to the passphrase.
+    #[clap(long, short = 's')]
+    with_special_char: bool,
+}
+
 fn main() {
-    let matches = App::new("diceware")
-        .version("1.0.1")
-        .author("Jean-Philippe Cugnet <jean-philippe@cugnet.eu>")
-        .about("A Diceware passphrase generator")
-        .arg(
-            Arg::with_name("words")
-                .help("The number of words to generate")
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("word_file")
-                .short('f')
-                .long("file")
-                .takes_value(true)
-                .help("Uses a diceware word file"),
-        )
-        .arg(
-            Arg::with_name("english")
-                .long("en")
-                .help("Uses the English embedded word list"),
-        )
-        .arg(
-            Arg::with_name("french")
-                .long("fr")
-                .help("Uses the French embedded word list"),
-        )
-        .group(
-            ArgGroup::with_name("word_list")
-                .arg("word_file")
-                .arg("english")
-                .arg("french"),
-        )
-        .arg(
-            Arg::with_name("with-special-char")
-                .long("with-special-char")
-                .short('s')
-                .help("Adds a special character to the passphrase"),
-        )
-        .get_matches();
+    let cli = Cli::parse();
 
-    let words_str = matches.value_of("words").unwrap();
-    let words = words_str.parse().unwrap_or_else(|_| {
-        eprintln!(
-            "Error: `{}` is not a valid number of words. Please use an integer \
-            instead.",
-            words_str
-        );
-
-        process::exit(1);
-    });
-
-    let word_file = matches.value_of("word_file");
-    let with_special_char = matches.is_present("with-special-char");
-
-    let config = if let Some(filename) = word_file {
-        Config::with_filename(filename, words, with_special_char)
+    let config = if let Some(ref filename) = cli.word_file {
+        Config::with_filename(filename, cli.words, cli.with_special_char)
     } else {
-        let list = if matches.is_present("english") {
+        let list = if cli.english {
             EmbeddedList::EN
-        } else if matches.is_present("french") {
+        } else if cli.french {
             EmbeddedList::FR
         } else {
             EmbeddedList::EN
         };
 
-        Config::with_embedded(list, words, with_special_char)
+        Config::with_embedded(list, cli.words, cli.with_special_char)
     };
 
     match diceware::make_passphrase(config) {
@@ -103,7 +71,7 @@ fn main() {
         Err(err) => {
             match err {
                 Error::IO(e) => {
-                    eprintln!("Error: {}: {}", word_file.unwrap(), e)
+                    eprintln!("Error: {}: {}", cli.word_file.unwrap(), e)
                 }
 
                 Error::WordList(e) => eprintln!("Error: {}", e),
